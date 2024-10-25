ae32abcfba1963c8a711a34b301f2d233e4a03a1
shaon508 <shaonroy54@gmail.com>
2024-10-26 00:13:46 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,75 +6,162 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-            primarySwatch: Colors.green,
-            appBarTheme: AppBarTheme(backgroundColor: Colors.blue,)),
-        color: Colors.green,
-        debugShowCheckedModeBanner: false,
-        home:  HomeActivity());
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      //theme: ThemeData(
+      // primarySwatch: Colors.white,
+      // ),
+      home: const HomeActivity(),
+    );
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  HomeActivity({super.key});
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
 
   @override
-  Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Home"),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-
-              OutlinedButton(
-                  onPressed: (){
-                    Navigator.push(context,MaterialPageRoute(builder: (context)=>Activity1()));
-                  },
-                  child: Text("Go to Activity 1"),
-              ),
-
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=>Activity2()));
-                },
-                child: Text("Go to Activity 2"),
-              )
-
-
-            ],
-          ),
-        );
+  State<HomeActivity> createState() {
+    return MyHomePageState();
   }
 }
 
-class Activity1 extends StatelessWidget {
-  Activity1({super.key});
+class MyHomePageState extends State<HomeActivity> {
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-       return Scaffold(
-         appBar: AppBar(
-           title: Text("This is activity 1"),
-         ),
-       );
-  }
-}
-
-class Activity2 extends StatelessWidget {
-  Activity2({super.key});
-
+  var result = "";
+  var bgColor = Colors.transparent;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("This is activity 2"),
-      ),
-    );
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: AppBar(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(30),
+            )),
+            backgroundColor: Colors.purpleAccent.shade700,
+            foregroundColor: Colors.black,
+            titleSpacing: 8,
+            toolbarOpacity: 1,
+            elevation: 10,
+            title: Center(
+              child: const Text(
+                "BMI CHECKER",
+                style: TextStyle(
+                    // color: Colors.green,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          color: bgColor,
+          child: Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Text(
+                  //  'BMI',
+                  //   style: TextStyle(fontSize: 34, fontWeight: FontWeight.w100),
+                  //  ),
+                  SizedBox(
+                    height: 21,
+                  ),
+                  TextField(
+                    controller: wtController,
+                    decoration: InputDecoration(
+                      label: Text('Enter your weight (in kg)'),
+                      prefixIcon: Icon(Icons.line_weight),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  TextField(
+                    controller: ftController,
+                    decoration: InputDecoration(
+                      label: Text('Enter your height (in feet)'),
+                      prefixIcon: Icon(Icons.height),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  TextField(
+                    controller: inController,
+                    decoration: InputDecoration(
+                      label: Text('Enter your height (in inch)'),
+                      prefixIcon: Icon(Icons.height),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 21,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      var wt = wtController.text.toString();
+                      var ft = ftController.text.toString();
+                      var inch = inController.text.toString();
+                      if (wt != "" && ft != "" && inch != "") {
+                        // main calculation
+
+                        var intWt = int.parse(wt);
+                        var intFt = int.parse(ft);
+                        var intInch = int.parse(inch);
+
+                        var totalInch = intFt * 12 + intInch;
+                        var convertToMeter = 0.0254 * totalInch;
+
+                        var bmi = intWt / (convertToMeter * convertToMeter);
+                        var msg = "";
+                        if (bmi < 18) {
+                          msg = "You're UnderWeight !";
+                          bgColor = Colors.yellowAccent.shade200;
+                        } else if (bmi > 25) {
+                          msg = "You're OverWeight !";
+                          bgColor = Colors.redAccent.shade200;
+                        } else {
+                          msg = "You're Healthy!";
+                          bgColor = Colors.greenAccent.shade200;
+                        }
+                        setState(() {
+                          result =
+                              "$msg \n Your BMI is : ${bmi.toStringAsFixed(2)}";
+                        });
+                      } else {
+                        setState(() {
+                          result = "Please fill the all required fill!";
+                        });
+                      }
+                    },
+                    child: Text("Calculate"),
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    result,
+                    style: TextStyle(fontSize: 19),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
